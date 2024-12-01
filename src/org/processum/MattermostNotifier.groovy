9552926edd5726d186dd934921205e7bc9dfa084
processum <150809907+processum@users.noreply.github.com>
2024-12-01 12:19:53 +0300

    debug

--- src/org/processum/MattermostNotifier.groovy
+++ src/org/processum/MattermostNotifier.groovy
@@ -4,10 +4,12 @@ import groovy.json.JsonOutput
 
 class MattermostNotifier {
 
+    // Method to send a message to Mattermost
     static void sendMessage(String url, String token, String channelId, String message, String color = 'good') {
+        // Prepare the payload
         def payload = [
             channel_id: channelId,
-            message:'',
+            message: '',
             props:  [
                 attachments: [
                     [
@@ -18,17 +20,21 @@ class MattermostNotifier {
             ]
         ]
 
-        def response = httpRequest(
-            httpMode: 'POST',
-            url: "${url}/api/v4/posts",
-            customHeaders: [[name: 'Authorization', value: "Bearer ${token}"]],
-            contentType: 'APPLICATION_JSON',
-            requestBody: JsonOutput.toJson(payload)
-        )
+        // Use the Jenkins pipeline 'httpRequest' step within a script block
+        def response = null
+        script {
+            response = httpRequest(
+                httpMode: 'POST',
+                url: "${url}/api/v4/posts",
+                customHeaders: [[name: 'Authorization', value: "Bearer ${token}"]],
+                contentType: 'APPLICATION_JSON',
+                requestBody: JsonOutput.toJson(payload)
+            )
+        }
 
+        // Check the response and handle errors if necessary
         if (response.status != 201) {
             error "Failed to send message to Mattermost. Response: ${response.content}"
         }
     }
-
 }
