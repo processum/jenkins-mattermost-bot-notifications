62f21b156b7ce491640a26d2afe41f8b0a6ccb81
processum <150809907+processum@users.noreply.github.com>
2024-12-01 17:08:20 +0300

    debug

--- vars/sendMattermostNotification.groovy
+++ vars/sendMattermostNotification.groovy
@@ -17,14 +17,11 @@ def call(String message, String color) {
         error "MATTERMOST_URL and MATTERMOST_CHANNEL_ID must be set as global environment variables!"
     }
 
-    def resolvedVar = sh(script: "bash -c 'echo ${message}'", returnStdout: true).trim()
-    def messageResolver = sh(script: "echo ${message}", returnStdout: true).trim()
-
     if (credentialsId) {
         // Use Jenkins Credentials if MATTERMOST_CREDENTIALS_ID is defined
         withCredentials([string(credentialsId: credentialsId, variable: 'MATTERMOST_BOT_TOKEN')]) {
             def token = MATTERMOST_BOT_TOKEN
-            notifier.sendMessage(url, token, channelId, messageResolver, color)
+            notifier.sendMessage(url, token, channelId, message, color)
             echo token
         }
     } else {
@@ -33,6 +30,6 @@ def call(String message, String color) {
         if (!token) {
             error "MATTERMOST_BOT_TOKEN must be set as a global environment variable if MATTERMOST_CREDENTIALS_ID is not provided!"
         }
-        notifier.sendMessage(url, token, channelId, messageResolver, color)
+        notifier.sendMessage(url, token, channelId, message, color)
     }
 }
